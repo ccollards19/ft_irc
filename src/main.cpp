761b0179ef27da2825b0f249abcf200a4da60fa3
ccollards19 <cy.collard@gmail.com>
2023-10-04 18:53:15 +0200

    added structs and now print msg received

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include <cstddef>
+#include <string>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <sys/event.h>
@@ -11,22 +13,66 @@
 #include <unistd.h>
 #include <iostream>
 #include <map>
+#include <vector>
 
+
+struct channel
+{
+// The channel identifier is a function of the time.  The current time
+//    (as defined under UNIX by the number of seconds elapsed since
+//    00:00:00 GMT, January 1, 1970) is converted in a string of five (5)
+//    characters using the following base:
+//    "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890" (each character has a decimal
+//    value starting from 0 for 'A' to 35 for '0').
+
+//    The channel identifier therefore has a periodicity of 36^5 seconds
+//    (about 700 days).
+  std::string chan_id;
+  char prefix;
+  struct user *creator;
+  std::vector<struct user *> members;
+  std::vector<struct user *> operators;
+// O - give "channel creator" status;
+// o - give/take channel operator privilege;
+// v - give/take the voice privilege;
+
+// a - toggle the anonymous channel flag;
+// i - toggle the invite-only channel flag;
+// m - toggle the moderated channel;
+// n - toggle the no messages to channel from clients on the
+//     outside;
+// q - toggle the quiet channel flag;
+// p - toggle the private channel flag;
+// s - toggle the secret channel flag;
+// r - toggle the server reop channel flag;
+// t - toggle the topic settable by channel operator only flag;
+
+// k - set/remove the channel key (password);
+// l - set/remove the user limit to channel;
+
+// b - set/remove ban mask to keep users out;
+// e - set/remove an exception mask to override a ban mask;
+// I - set/remove an invitation mask to automatically override
+//     the invite-only flag;
+  std::string mode;
+};
 
 struct user
 {
-	int perm;
+	std::string mode;
 };
 
 struct server
 {
+  std::string server_id;
 	struct sockaddr		_sock_addr;
 	socklen_t		_socklen;
 	int			_socketfd;
+
 	struct kevent		_changelist;
 	struct kevent		_eventlist[2];
-	int 			_kq;
 	struct timespec 	_timeout;
+	int 			_kq;
 
 	std::map<int, user*>	_connections;
 
@@ -52,9 +98,9 @@ void server::add_connection()
 {
 	std::cout<<"new connection"<<std::endl;
 	int newfd  = accept(_socketfd, &(_sock_addr), &(_socklen));
-	(_connections)[newfd] = new user();
-	EV_SET(&(_changelist), newfd, EVFILT_READ, EV_ADD | EV_ENABLE | EV_CLEAR, 0, 0, NULL);
-	if (kevent(_kq, &(_changelist), 1, NULL, 0, &(_timeout)) == -1)
+	_connections[newfd] = new user();
+	EV_SET(&_changelist, newfd, EVFILT_READ, EV_ADD | EV_ENABLE | EV_CLEAR, 0, 0, NULL);
+	if (kevent(_kq, &_changelist, 1, NULL, 0, &_timeout) == -1)
 	{
 		std::cerr<<"kqueue error durring server startup "<<std::endl
 		<<"error: "<<strerror(errno)<<std::endl;
@@ -64,7 +110,11 @@ void server::add_connection()
 
 void server::receive_message()
 {
+  char *buffer = (char *)malloc(sizeof(char) * (size_t)(_eventlist[0].data));
 	std::cout<<"received message on fd : "<<_eventlist[0].ident<<std::endl;
+  recv(_eventlist[0].ident, buffer,(size_t)(_eventlist[0].data), 0);
+	std::cout<<buffer<<std::endl;
+  free(buffer);
 }
 
 void server::send_message()
@@ -81,8 +131,7 @@ void server::run()
 		nbr_event = kevent(_kq, NULL, 0, reinterpret_cast<struct kevent *>(_eventlist), 2, &(_timeout));
 		if (nbr_event == -1)
 		{
-			std::cerr<<"kqueue error durring runtime"<<std::endl
-			<<"error: "<<strerror(errno)<<std::endl;
+			std::cerr<<"kqueue error durring runtime"<<std::endl<<"error: "<<strerror(errno)<<std::endl;
 			safe_shutdown(EXIT_FAILURE);
 		}
 		for (int i = 0; i < nbr_event; i++)
