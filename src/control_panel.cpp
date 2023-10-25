014180cb04f0fa3bbfa2addde5967cd6e155c026
Nicolas ESPANA RIBERA <nico@Nicolass-MacBook-Pro.local>
2023-10-25 17:16:21 +0200

    ERROR_BY_CMD

--- src/control_panel.cpp
+++ src/control_panel.cpp
@@ -6,33 +6,46 @@ void send_error(server &s,client &c, std::string message)
 	c._send_buffer.append(message);
 	s.write_set(c._fd);
 }
+
 void handle_error_message(Message &m, server &s, client &c, int error)
 {
+	int nick;
+	int server;
+	int chan;
+	int other;
+	switch (m.getCommand()) {
+		case KICK : nick = 1; server = 0; chan = 0; other = 0;break;
+		case INVITE : nick = 0; server = 0; chan = 1; other = 0;break;
+		case TOPIC : nick = 0; server = 0; chan = 0; other = 1;break;
+		case PRIVMSG : nick = 0; server = 0; chan = 0; other = 1;break;
+		case JOIN : nick = 0; server = 0; chan = 0; other = 1;break;
+		default : nick = 0; server = 0; chan = 0; other = 0;
+	}
 	switch (error) {
-		case ERR_NOSUCHNICK : send_error(s, c, m.getContent()[0] + " :No such nick/channel\n");break;
-		case ERR_NOSUCHSERVER : send_error(s, c, m.getContent()[0] + " :No such server\n");break;
-		case ERR_NOSUCHCHANNEL : send_error(s, c, m.getContent()[0] + " :No such channel\n");break;
-		case ERR_CANNOTSENDTOCHAN  : send_error(s, c, m.getContent()[0] + " :Cannot send to channel\n ");break;
-		case ERR_TOOMANYCHANNELS  : send_error(s, c, m.getContent()[0] + " :You have joined too many channels\n");break;
-		case ERR_WASNOSUCHNICK  : send_error(s, c, m.getContent()[0] + " :There was no such nickname");break;
-		case ERR_TOOMANYTARGETS  : send_error(s, c, m.getContent()[0] + " :Duplicate recipients. No message delivered\n");break;
+		case ERR_NOSUCHNICK : send_error(s, c, m.getContent()[nick] + " :No such nick/channel\n");break;
+		case ERR_NOSUCHSERVER : send_error(s, c, m.getContent()[server] + " :No such server\n");break;
+		case ERR_NOSUCHCHANNEL : send_error(s, c, m.getContent()[chan] + " :No such channel\n");break;
+		case ERR_CANNOTSENDTOCHAN  : send_error(s, c, m.getContent()[chan] + " :Cannot send to channel\n ");break;
+		case ERR_TOOMANYCHANNELS  : send_error(s, c, m.getContent()[chan] + " :You have joined too many channels\n");break;
+		case ERR_WASNOSUCHNICK  : send_error(s, c, m.getContent()[nick] + " :There was no such nickname");break;
+		case ERR_TOOMANYTARGETS  : send_error(s, c, m.getContent()[other] + " :Duplicate recipients. No message delivered\n");break;
 		case ERR_NOORIGIN  : send_error(s, c, ":No origin specified ");break;
 		case ERR_NORECIPIENT  : send_error(s, c, ":No recipient given " + m.getCommandName() + "\n");break;
 		case ERR_NOTEXTTOSEND  : send_error(s, c, ":No text to send\n");break;
-		case ERR_NOTOPLEVEL  : send_error(s, c, m.getContent()[0] + " :No top level domain specified\n");break;
-		case ERR_WILDTOPLEVEL  : send_error(s, c, m.getContent()[0] + " :Wildcard in toplevel domain\n");break;
+		case ERR_NOTOPLEVEL  : send_error(s, c, m.getContent()[other] + " :No top level domain specified\n");break;
+		case ERR_WILDTOPLEVEL  : send_error(s, c, m.getContent()[other] + " :Wildcard in toplevel domain\n");break;
 		case ERR_UNKNOWNCOMMAND  : send_error(s, c, m.getCommandName() + " :Unknown command\n");break;
 		case ERR_NOMOTD  : send_error(s, c, ":MOTD File is missing\n");break;
 		case ERR_NOADMININFO  : send_error(s, c, s._servername + " :No administrative info available\n");break;
 		case ERR_FILEERROR  : send_error(s, c, ":File error doing " + m.getContent()[0] + " on " + m.getContent()[1] + "\n");break;
 		case ERR_NONICKNAMEGIVEN  : send_error(s, c, ":No nickname given\n");break;
-		case ERR_ERRONEUSNICKNAME  : send_error(s, c, m.getContent()[0] + " :Erroneus nickaname\n");break;
-		case ERR_NICKNAMEINUSE  : send_error(s, c, m.getContent()[0] + " :Nickname is already in use\n");break;
-		case ERR_NICKCOLLISION  : send_error(s, c, m.getContent()[0] + " :Nickname collision KILL\n");break;
-		case ERR_USERNOTINCHANNEL  : send_error(s, c, "");break; //TODO ask Cyril to add the channel provenance
-		case ERR_NOTONCHANNEL  : send_error(s, c, m.getContent()[1] +" :You're not on that channel\n");break;
-		case ERR_USERONCHANNEL  : send_error(s, c, m.getContent()[0] + " " + m.getContent()[1]+" :is already on channel\n");break;
-		case ERR_NOLOGIN  : send_error(s, c, m.getContent()[0] + " :User not logged in\n");break;
+		case ERR_ERRONEUSNICKNAME  : send_error(s, c, m.getContent()[nick] + " :Erroneus nickaname\n");break;
+		case ERR_NICKNAMEINUSE  : send_error(s, c, m.getContent()[nick] + " :Nickname is already in use\n");break;
+		case ERR_NICKCOLLISION  : send_error(s, c, m.getContent()[nick] + " :Nickname collision KILL\n");break;
+		case ERR_USERNOTINCHANNEL  : send_error(s, c, m.getContent()[nick] + " " + m.getContent()[chan] +" :They aren't on that channel\n");break;
+		case ERR_NOTONCHANNEL  : send_error(s, c, m.getContent()[chan] +" :You're not on that channel\n");break;
+		case ERR_USERONCHANNEL  : send_error(s, c, m.getContent()[chan] + " " + m.getContent()[1]+" :is already on channel\n");break;
+		case ERR_NOLOGIN  : send_error(s, c, m.getContent()[nick] + " :User not logged in\n");break;
 		case ERR_SUMMONDISABLED  : send_error(s, c, ":SUMMON has been disabled\n");break;
 		case ERR_USERSDISABLED  : send_error(s, c, ":USERS has been disabled\n");break;
 		case ERR_NOTREGISTERED  : send_error(s, c, ":You have not registered\n");break;
@@ -41,14 +54,14 @@ void handle_error_message(Message &m, server &s, client &c, int error)
 		case ERR_NOPERMFORHOST  : send_error(s, c, ":Your host isn't among the privileged");break;
 		case ERR_PASSWDMISMATCH  : send_error(s, c, ":Password incorrect\n");break;
 		case ERR_YOUREBANNEDCREEP  : send_error(s, c, "You are banned from this server\n");break;
-		case ERR_KEYSET  : send_error(s, c, m.getContent()[0] + ":Channel key already set\n");break;
-		case ERR_CHANNELISFULL  : send_error(s, c, m.getContent()[0] + ":Cannot join channel (+l)");break;
-		case ERR_UNKNOWNMODE  : send_error(s, c, m.getContent()[0] + ":is unknown mode char to me");break;
-		case ERR_INVITEONLYCHAN  : send_error(s, c, m.getContent()[0] + ":Cannot join channel (+i)");break;
-		case ERR_BANNEDFROMCHAN  : send_error(s, c, m.getContent()[0] + ":Cannot join channel (+b)");break;
-		case ERR_BADCHANNELKEY  : send_error(s, c, m.getContent()[0] + ":Cannot join channel (+k)");break;
+		case ERR_KEYSET  : send_error(s, c, m.getContent()[chan] + ":Channel key already set\n");break;
+		case ERR_CHANNELISFULL  : send_error(s, c, m.getContent()[chan] + ":Cannot join channel (+l)");break;
+		case ERR_UNKNOWNMODE  : send_error(s, c, m.getContent()[chan] + ":is unknown mode char to me");break;
+		case ERR_INVITEONLYCHAN  : send_error(s, c, m.getContent()[chan] + ":Cannot join channel (+i)");break;
+		case ERR_BANNEDFROMCHAN  : send_error(s, c, m.getContent()[chan] + ":Cannot join channel (+b)");break;
+		case ERR_BADCHANNELKEY  : send_error(s, c, m.getContent()[chan] + ":Cannot join channel (+k)");break;
 		case ERR_NOPRIVILEGES  : send_error(s, c, ":Permission Denied- You're not an IRC operator\n");break;
-		case ERR_CHANOPRIVSNEEDED  : send_error(s, c, "");break; //TODO Cyr
+		case ERR_CHANOPRIVSNEEDED  : send_error(s, c, "");break;
 		case ERR_CANTKILLSERVER  : send_error(s, c, ":You can't kill a server\n");break;
 		case ERR_NOOPERHOST  : send_error(s, c, "No O-lines for your host\n");break;
 		case ERR_UMODEUNKNOWNFLAG  : send_error(s, c, ":Unknown MODE flag\n");break;
